84be35c3257c850dd5b7696bac5e8760727ff0a0
Brian Rahadi <brian.rahadi@gmail.com>
2022-09-01 23:38:44 -0700

    added hover effect on PersonCard

--- components/PersonCard.jsx
+++ components/PersonCard.jsx
@@ -1,18 +1,40 @@
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
+import { useState } from "react";
 
-function PersonCard({key, name, description}) {
-  const image = require('public/brian-rahadi.jpeg')
+const PersonCard = ({ key, name, description }) => {
+  const [isHovered, setHover] = useState(false);
+
+  const image = require("public/brian-rahadi.jpeg");
   return (
-    <Card style={{ width: '18rem', display:'inline-block', margin:'1.5em', flexShrink: '0'}} key={key}>
-      <Card.Img variant="top" src="https://brianrahadi.github.io/brian-rahadi.jpg" />
-      <Card.Body style={{textAlign: 'center'}}>
-        <Card.Title>{ name }</Card.Title>
-        <Card.Text>
-          { description }
-        </Card.Text>
+    <Card
+      style={{
+        width: "18rem",
+        display: "inline-block",
+        margin: "1.5em",
+        flexShrink: "0",
+      }}
+      key={key}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <Card.Img
+        variant="top"
+        src={
+          isHovered
+            ? "https://brianrahadi.github.io/brian-rahadi.jpg"
+            : "/sfuia-about.jpeg"
+        }
+        height="350"
+        style={{
+          objectFit: "cover"
+        }}
+      />
+      <Card.Body style={{ textAlign: "center" }}>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
       </Card.Body>
     </Card>
   );
-}
+};
 
 export default PersonCard;
