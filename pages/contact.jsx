48fa7966e174e316222d47cafdb4282cd3fadb8e
Brian Rahadi <brian.rahadi@gmail.com>
2022-09-13 23:03:59 -0700

    added message in contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,16 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import EventCard from "components/EventCard";
-import { Container } from "react-bootstrap";
-import { Fade } from "react-awesome-reveal";
-import pastEvents from "data/pastEvents.json";
-import upcomingEvents from "data/upcomingEvents.json";
-import Banner from "components/Banner";
-
 export default function Team() {
   return (
-    <div>
-
-    </div>
+    <h1 style={{textAlign: 'center'}}>To Be Added</h1>  
   );
 }
