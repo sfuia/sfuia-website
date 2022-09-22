d3c36f6cb29ac3dff5a7c8b96bd0303cf7a63ecd
git <git@github.com>
2022-09-21 20:09:20 -0700

    added styling to contact form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import { Fade } from "react-awesome-reveal";
 import Banner from "components/Banner";
+import styles from '../styles/contactForm.module.scss'
 
 export default function Contact() {
   async function handleOnSubmit(e){
@@ -9,12 +10,13 @@ export default function Contact() {
     Array.from(e.currentTarget.elements).forEach(field => {
         if(!field.name) return;
         formData[field.name] = field.value;
+        field.value="";
     });
     fetch('api/mail',{
       method: 'post',
       body: JSON.stringify(formData)
     })
-    console.log(formData)
+    
   }
 
   return (
@@ -35,13 +37,14 @@ export default function Contact() {
               margin: "3em auto",
             }}
           >
-          <h3 style={{marginBottom:"3rem"}}>Have a question you want to ask the SFUIA team? Feel free to contact us!</h3>
+          <h2>Contact Us</h2>
+          <p>Have a question you want to ask the SFUIA team? Feel free to contact us!</p>
           <form method="post" onSubmit={handleOnSubmit}>
-              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="name" placeholder="Full name"/><br></br>
-              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="email" placeholder="Email"/><br></br>
-              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="subject" placeholder="Subject"/><br></br>
-              <textarea style={{padding:'0.5rem', margin:"0.2rem", width:"750px", height:"200px", backgroundColor:"#E2F8FF"}} type="text"name="message" placeholder="Message"/><br></br>
-            <input style={{padding:'0.5rem', margin:"0.2rem"}} type="submit" value="Submit"/>
+              <input className={styles.input} type="text" name="name" placeholder="Full name"/><br></br>
+              <input className={styles.input} type="text" name="email" placeholder="Email"/><br></br>
+              <input className={styles.input} type="text" name="subject" placeholder="Subject"/><br></br>
+              <textarea className={styles.message} type="text"name="message" placeholder="Message"/><br></br>
+            <input className={styles.submit} type="submit" value="Submit"/>
           </form>
         </Container>
       </Fade>
