858c91647d049299e91455560d57ac3c59c8b158
git <git@github.com>
2022-09-21 18:35:27 -0700

    implemented simple contact form

---
+++ pages/api/mail.js
@@ -0,0 +1,26 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default function handler(req, res) {
+    const body = JSON.parse(req.body);
+    const message = `
+        Name: ${body.name}\r\n
+        Email: ${body.email}\r\n
+        Message: ${body.message}
+    `;
+    const subject = `Subject: ${body.subject}`;
+
+    const data = {
+        to: '02c.albert@gmail.com',
+        from: 'contact@sfuia.com',
+        subject: subject,
+        text: message,
+        html: message.replace(/\r\n/g, '<br>')
+    }
+
+    mail.send(data);
+
+    res.status(200).json({ status:'OK' })
+  }
+  
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,51 @@
-export default function Team() {
+import { Container } from "react-bootstrap";
+import { Fade } from "react-awesome-reveal";
+import Banner from "components/Banner";
+
+export default function Contact() {
+  async function handleOnSubmit(e){
+    e.preventDefault();
+    const formData = {}
+    Array.from(e.currentTarget.elements).forEach(field => {
+        if(!field.name) return;
+        formData[field.name] = field.value;
+    });
+    fetch('api/mail',{
+      method: 'post',
+      body: JSON.stringify(formData)
+    })
+    console.log(formData)
+  }
+
   return (
-    <h1 style={{textAlign: 'center'}}>To Be Added</h1>  
+    <div className="contact">
+      <Fade triggerOnce>
+      <Banner
+        imageSrc="/sfuia-team.jpeg"
+        text="Contact Us"
+        height="50"
+      />
+        <Container
+            style={{
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center", 
+              flexDirection: "column",
+              margin: "3em auto",
+            }}
+          >
+          <h3 style={{marginBottom:"3rem"}}>Have a question you want to ask the SFUIA team? Feel free to contact us!</h3>
+          <form method="post" onSubmit={handleOnSubmit}>
+              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="name" placeholder="Full name"/><br></br>
+              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="email" placeholder="Email"/><br></br>
+              <input style={{padding:'0.5rem', margin:"0.2rem", width:"750px", backgroundColor:"#E2F8FF"}} type="text" name="subject" placeholder="Subject"/><br></br>
+              <textarea style={{padding:'0.5rem', margin:"0.2rem", width:"750px", height:"200px", backgroundColor:"#E2F8FF"}} type="text"name="message" placeholder="Message"/><br></br>
+            <input style={{padding:'0.5rem', margin:"0.2rem"}} type="submit" value="Submit"/>
+          </form>
+        </Container>
+      </Fade>
+    </div>
+    
   );
 }
