d085bb718f3ba87a9cbe39ed6887e7840ef048b8
calbertan <02c.albert@gmail.com>
2023-09-26 22:13:46 -0700

    change apostrophe

--- components/EventCarousel.jsx
+++ components/EventCarousel.jsx
@@ -11,7 +11,7 @@ const EventCarousel = () => {
           height="50"
         />
           <Carousel.Caption>
-            <h3>✨ Let’s Meet In Person ✨</h3>
+            <h3>✨ Let&#39;s s Meet In Person ✨</h3>
             <p>🌟 Join us at SFUIA's LEMPER Welcoming Party! 🤝 Get ready for games and exciting activities to make new friends and have a blast. 🎉</p>
           </Carousel.Caption>
       </Carousel.Item>
@@ -23,7 +23,7 @@ const EventCarousel = () => {
         />
           <Carousel.Caption>
             <h3>Our food sale ensures you don't miss the taste of home 🇮🇩</h3>
-            <p>Don't miss out on preordering our GEROBAK’s delicious nasi kulit & telor balado for $11 before 12 pm on Friday, September 29th. 🍛</p>
+            <p>Don't miss out on preordering our GEROBAK&#39;s s delicious nasi kulit & telor balado for $11 before 12 pm on Friday, September 29th. 🍛</p>
           </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
