2decfd5dadfd7e63ddec1f6f592da9c2c83299ca
Christopher Tan <02c.albert@gmail.com>
2023-12-03 17:55:42 -0800

    calendar follows sfu.indonesia google calendar

--- components/EventCalendar.jsx
+++ components/EventCalendar.jsx
@@ -17,64 +17,38 @@ const localizer = dateFnsLocalizer({
   locales
 });
 
-const events = [
-  {
-    id: 0,
-    title: 'Golf @Burnaby Mountain Golf',
-    start: new Date(2023, 9, 21, 13, 30),
-    end: new Date(2023, 9, 21, 16, 30),
-  },
-  {
-    id: 1,
-    title: 'Gerobak - Bakmi Ayam + Pangsit',
-    start: new Date(2023, 9, 23, 0, 0),
-    end: new Date(2023, 9, 26, 24, 0),
-    color: 'red'
-  },
-  {
-    id: 2,
-    title: 'Badminton @ Badminton Vancouver',
-    start: new Date(2023, 10, 11, 14, 0),
-    end: new Date(2023, 10, 11, 16, 0),
-  },
-  {
-    id: 4,
-    title: 'Gerobak - Soto Betawi + Perkedel',
-    start: new Date(2023, 10, 18, 16, 0),
-    end: new Date(2023, 10, 18, 16, 0),
-  },
-];
-
 const handleEventClick = (event) => {
   let eventLink = ""
-  if (event.id % 2 == 0) {
-    eventLink = "https://docs.google.com/forms/d/e/1FAIpQLSdPCbY78ceVVbpvg7fmdSxsocQdtwLWXh9Mig0agR9iURmkNA/viewform?usp=sf_link";
-  } else{
+  console.log(event.title)
+  if (event.title.toLowerCase().includes('gerobak'))
+  {
     eventLink = "https://docs.google.com/forms/d/1mXJsFCj9lH4mk4jkizC3mqLYh9CYSvh5HcE7_nrBY70/prefill";
+  } else{
+    eventLink = "https://docs.google.com/forms/d/e/1FAIpQLSdPCbY78ceVVbpvg7fmdSxsocQdtwLWXh9Mig0agR9iURmkNA/viewform?usp=sf_link";
   }
   window.open(eventLink, '_blank');
 };
 
 export default function EventCalendar(){
-  // const calendarURL =
-  //   "https://clients6.google.com/calendar/v3/calendars/j7qfcngd9crbhelib6tgdihi3k@group.calendar.google.com/events?calendarId=j7qfcngd9crbhelib6tgdihi3k%40group.calendar.google.com&singleEvents=true&timeZone=America%2FVancouver&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2023-03-26T00%3A00%3A00-07%3A00&timeMax=2024-05-07T00%3A00%3A00-07%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs";
-  // const [events, setEvents] = useState([]);
+  const calendarID = 'a1be84a17449db9630d39f7be12c710bff12b2cd5f138b05c0ff8027efb22327@group.calendar.google.com'
+  const calendarURL = `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events?key=AIzaSyAjQzkXxpzJomSLSnmPBZGSBC1JnNlxg7o`
+  const [events, setEvents] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(calendarURL)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const updatedEvents = [];
-  //       for (const event of data.items) {
-  //         updatedEvents.push({
-  //           title: event.summary,
-  //           start: new Date(event.start.dateTime),
-  //           end: new Date(event.end.dateTime),
-  //         });
-  //       }
-  //       setEvents(updatedEvents);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(calendarURL)
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedEvents = [];
+        for (const event of data.items) {
+          updatedEvents.push({
+            title: event.summary,
+            start: new Date(event.start.dateTime),
+            end: new Date(event.end.dateTime),
+          });
+        }
+        setEvents(updatedEvents);
+      });
+  }, []);
 
   return (
     <div className='max-w-7xl m-auto'>
